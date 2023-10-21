45ccc63ca2fe6af08e5e5f62bc434f853b0ff5a1
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-10-21 17:48:24 +0000

    TailwindCSS works!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-green-500 p-4">
+      <h1 className="text-white text-center">TailwindCSS is Working!</h1>
     </div>
   );
 }
