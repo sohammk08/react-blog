1b873986c79dc08b8b95f4c7f7fd1cbdc0bfb275
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-10-26 11:09:55 +0000

    Added: rsuite, Dropdown

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,39 @@
+import "rsuite/dist/rsuite.min.css";
+import Dropdown from "rsuite/Dropdown";
+import GearIcon from "@rsuite/icons/Gear";
+import { db, auth } from "../firebase";
+import { useState, useEffect } from "react";
+import UserBadgeIcon from "@rsuite/icons/UserBadge";
+import UserInfoIcon from "@rsuite/icons/UserInfo";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+function DropDown() {
+  const [username, setUsername] = useState();
+
+  useEffect(() => {
+    const user = auth.currentUser.email;
+    const q = query(collection(db, "users"), where("email", "==", user));
+    const rData = async () => {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUsername(doc.data().username);
+      });
+    };
+    rData();
+  }, []);
+
+  return (
+    <div>
+      <Dropdown title={username} icon={<UserBadgeIcon />}>
+        <Dropdown.Item icon={<UserInfoIcon />} as="a">
+          Profile
+        </Dropdown.Item>
+        <Dropdown.Item icon={<GearIcon />} as="a">
+          Settings
+        </Dropdown.Item>
+      </Dropdown>
+    </div>
+  );
+}
+
+export default DropDown;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import DropDown from "./DropDown";
+import { auth } from "../firebase";
 import { Link } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function Header() {
+  const [userState, setUserState] = useState(false);
+
+  // Check the authentication state. If user is signed in, update the user state.
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserState(true); // User is signed in
+    } else {
+      console.log("No user is signed in"); // No user is signed in
+    }
+  });
+
+  // Signout function
+  const signUserOut = () => {
+    signOut(auth);
+    window.location.reload(false);
+  };
+
   return (
     <div className="w-full h-12 bg-white sticky top-0 flex justify-center font-jsans">
       <div className="items-center justify-center">
@@ -13,28 +33,50 @@ function Header() {
       <div className="flex items-center justify-right ml-auto">
         <ul className="flex justify-center list-none m-0 p-0">
           <li className="mr-5 text-lg font-light cursor-pointer">
-            <Link className="link" to="/">HOME</Link>
+            <Link className="link" to="/">
+              HOME
+            </Link>
           </li>
 
           <li className="mr-5 text-lg font-light cursor-pointer">
-            <Link className="link" to="/about">ABOUT</Link>
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
           </li>
 
           <li className="mr-5 text-lg font-light cursor-pointer">
-            <Link className="link" to="/write">WRITE</Link>
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+
+          <li
+            className="mr-5 text-lg font-light cursor-pointer"
+            onClick={signUserOut}
+          >
+            {userState && "LOGOUT"}
           </li>
         </ul>
       </div>
 
       <div className="flex items-center justify-right ml-auto">
-        <ul className="flex justify-center list-none m-0 p-0">
-          <li className="mr-5 text-lg font-light cursor-pointer">
-            <Link className="link" to="/login">LOGIN</Link>
-          </li>
-          <li className="mr-5 text-lg font-light cursor-pointer">
-            <Link className="link" to="/register">REGISTER </Link>
-          </li>
-        </ul>
+        {userState ? (
+          <DropDown />
+        ) : (
+          <ul className="flex justify-center list-none m-0 p-0">
+            <li className="mr-5 text-lg font-light cursor-pointer">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="mr-5 text-lg font-light cursor-pointer">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        ;
       </div>
     </div>
   );
