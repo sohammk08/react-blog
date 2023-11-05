a852a586f35f301f040c79ec3abbce68390528da
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-11-05 15:45:15 +0000

    feat: User Profile
    Added logic for fetching data and tailwindcss styling in Author.jsx | Added link for importing fonts in index.html | Created corresponding items in tailwind.config.js

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import { db, auth } from "../firebase";
+import { useState, useEffect } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function Author() {
+  const [userInfo, setUserInfo] = useState([]);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    const q = query(collection(db, "users"), where("email", "==", user.email));
+    const rData = async () => {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUserInfo(doc.data());
+      });
+    };
+    rData();
+  }, []);
+
   return (
-    <div>
-        Author
+    <div className="author">
+        <div className="twoLiFlex">
+          <div className="flex mt-2rem mr-0 mb-2rem ml-2rem">
+            <img 
+              src="https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              className="w-56 float-left rounded-md"
+              alt="" />
+            <h2 className="text-black text-4em font-jsans mt-auto mr-0 mb-auto ml-2rem">{userInfo.username}</h2>
+            <a className="text-black text-4em mt-5.5rem mr-0 mb-0 ml-2rem" href={userInfo.iglink}><i className="fa-brands fa-instagram"></i></a>
+          </div>
+          <div className="flex ml-24">
+            <ul className="text-center mt-auto mb-auto list-none">
+              <li className="cursor-pointer">Travel</li>
+              <li className="cursor-pointer">Food</li>
+              <li className="cursor-pointer">Fashion</li>
+            </ul>
+            <p className="text-black text-1em text-left font-vround ml-32 max-w-screen-xl">{userInfo.aboutme}</p>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Author;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,13 @@ module.exports = {
         '1em': '1em',
         '1.8em': '1.8em',
         '3.5rem': '3.5rem',
+        '4em': '4em',
       },
       spacing: {
         'vh-50': 'calc(100vh - 50px)',
         '70vw': '70vw',
+        '2rem': '2rem',
+        '5.5rem': '5.5rem',
       },
     },
   },
