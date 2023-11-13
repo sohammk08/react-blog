dcbca83407d5945ffc6baf107c53ea49394013ff
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-11-13 18:20:44 +0000

    page: Profile (tailwindcss styling)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,24 +32,24 @@ function Profile() {
           return (
             <div className="m-6 w-1/3 sm:w-1/3 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
               <img
-                className="postImage"
+                className="w-full h-280 objec-cover rounded-md"
                 src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                 alt=""
               />
-              <div className="postInfo">
+              <div className="flex flex-col items-center">
                 <div className="postCats">
-                  <span className="postCat">Music</span>
-                  <span className="postCat">Life</span>
+                  <span className="text-11 text-yellow-600 leading-5 font-vround mt-4 mr-2 cursor-pointer">Music</span>
+                  <span className="text-11 text-yellow-600 leading-5 font-vround mt-4 mr-2 cursor-pointer">Life</span>
                 </div>
-                <span className="postTitle">
+                <span className="text-2xl font-jsans font-bold mt-4 cursor-pointer">
                   {myBlogInformation.blog_title}
                 </span>
-                <span className="postAuthor">
+                <span className="text-base font-jsans font-normal mt-15 cursor-pointer">
                   {myBlogInformation.blog_author}
                 </span>
-                <span className="postDate">1 hour ago</span>
+                <span className="text-gray-600 italic text-xs p-0 font-lora">1 hour ago</span>
               </div>
-              <p className="postDesc">{myBlogInformation.blog_content}</p>
+              <p className="text-slate-900 text-sm leading-6 font-vround">{myBlogInformation.blog_content}</p>
             </div>
           );
         })}
