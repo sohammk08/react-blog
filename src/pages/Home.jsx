5754c7691e4ae2fe8a4f7a12e85316708a39a28e
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-11-16 12:16:46 +0000

    feat: Fetching blogs in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import { db } from "../firebase";
+import { useState, useEffect } from "react";
 import BlogCard from "../components/BlogCard";
+import { collection, getDocs } from "firebase/firestore";
 
 function Home() {
+  const [blogPostInfo, setBlogPostInfo] = useState([]);
+
+  useEffect(() => {
+    const getBlogInfo = async () => {
+      const data = await getDocs(collection(db, "blogs"));
+      setBlogPostInfo(data.docs.map((doc => ({...doc.data(), id: doc.id}))))
+    }
+    getBlogInfo();
+  })
+
   return (
-    <>
-      <h1 className="text-2xl font-vround text-center mb-4">Home</h1>
-      <BlogCard
-        imageUrl="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        author="Ashley Vance"
-        title="Hello World"
-        uploadTime="1 hour ago"
-        content="Hello World with Ashley Vance is a bloomberg originals series where the host Ashlee Vance goes around the world exploring various sorts technologies and sciences."
-      />
-    </>
-  );
+    <div className="blogPosts">
+      {blogPostInfo.map((postInfo) => {
+        return (
+            <>
+              <h1 className="text-2xl font-vround text-center mb-4">Home</h1>
+              <BlogCard
+                imageUrl="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                author={postInfo.blog_author}
+                title={postInfo.blog_title}
+                uploadTime="1 hour ago"
+                content={postInfo.blog_content}
+              />
+            </>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Home;
