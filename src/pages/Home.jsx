80a0eddaf8da9e16885133793d66db89003080dd
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-11-17 17:39:28 +0000

    fix: Changed the position of the title h1 so that it does not repeat with every bllog that is being fetched

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,29 +9,29 @@ function Home() {
   useEffect(() => {
     const getBlogInfo = async () => {
       const data = await getDocs(collection(db, "blogs"));
-      setBlogPostInfo(data.docs.map((doc => ({...doc.data(), id: doc.id}))))
-    }
+      setBlogPostInfo(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
     getBlogInfo();
-  })
+  });
 
   return (
-    <div className="blogPosts">
-      {blogPostInfo.map((postInfo) => {
-        return (
-            <>
-              <h1 className="text-2xl font-vround text-center mb-4">Home</h1>
-              <BlogCard
-                imageUrl="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                author={postInfo.blog_author}
-                title={postInfo.blog_title}
-                uploadTime="1 hour ago"
-                content={postInfo.blog_content}
-              />
-            </>
-        )
-      })}
-    </div>
-  )
+    <>
+      <h1 className="text-2xl font-vround text-center mb-4">Home</h1>
+      <div className="blogPosts">
+        {blogPostInfo.map((postInfo) => {
+          return (
+            <BlogCard
+              imageUrl="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              author={postInfo.blog_author}
+              title={postInfo.blog_title}
+              uploadTime="1 hour ago"
+              content={postInfo.blog_content}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Home;
